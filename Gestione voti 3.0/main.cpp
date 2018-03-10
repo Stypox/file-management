5a556b5a06025e9289b3c49de2afba72f243d1a0
Stypox <stypox@pm.me>
2018-03-10 21:11:44 +0100

    Added append functions overloaded for basic types and some others that append at the end of the file. Added add functions overloaded for basic types and some others that insert in a specific position. Added delete functions to delete sections of chars/words/lines. Completed operator<< that work with  basic types and some others.

--- Gestione voti 3.0/main.cpp
+++ Gestione voti 3.0/main.cpp
@@ -10,34 +10,34 @@ fIle >> test;
 fIle.seekg(0);
 fIle.put('b');
 
-if (!inFile.open()) return 0;
-inFile.file.put('t');
-inFile.pointTo(0, 0, 0);
-inFile.file >> test;
+if (!f1.open()) return 0;
+f1.file.put('t');
+f1.pointTo(0, 0, 0);
+f1.file >> test;
 std::cout << test;
-inFile.file >> test;
-std::cout << test << inFile.countWords("ciao, come va ?");
+f1.file >> test;
+std::cout << test << f1.countWords("ciao, come va ?");
 
-inFile.pointTo(1, 1, 0);
-inFile.file >> test;
+f1.pointTo(1, 1, 0);
+f1.file >> test;
 std::cout << test;
 
-inFile.pointTo(0, 0, 0);
-std::cout << inFile.file.tellg() << inFile.file.tellp();
-inFile.file.put('T');
+f1.pointTo(0, 0, 0);
+std::cout << f1.file.tellg() << f1.file.tellp();
+f1.file.put('T');
 
-inFile.file.close();
+f1.file.close();
 */
 /*
 std::cout << "l, w, c = lines, words, chars | nr = numero(di)\n\n";
-std::cout << "Linee:" << inFile.getNrLines() << " | Parole:" << inFile.getNrWords() << " | Byte:" << inFile.getNrChars() << "\n\n";
-std::cout << "nrw_l4:" << inFile.getNrWordsLine(3) << " | nrc_l4:" << inFile.getNrCharsLine(3) << " | nrc_w8:" << inFile.getNrCharsWord(7) << " | nrc_l3w5:" << inFile.getNrCharsWord(2, 4) << "\n\n";
-std::cout << "l6:" << inFile.getLine(5) << " | w3:" << inFile.getWord(2) << " | l3w2:" << inFile.getWord(2, 1) << " | c1:" << inFile.getChar(0)
-<< " | l2c3:" << inFile.getChar(1, 2) << " | l4w3c5:" << inFile.getChar(3, 2, 4) << "\n\n";
+std::cout << "Linee:" << f1.getNrLines() << " | Parole:" << f1.getNrWords() << " | Byte:" << f1.getNrChars() << "\n\n";
+std::cout << "nrw_l4:" << f1.getNrWordsLine(3) << " | nrc_l4:" << f1.getNrCharsLine(3) << " | nrc_w8:" << f1.getNrCharsWord(7) << " | nrc_l3w5:" << f1.getNrCharsWord(2, 4) << "\n\n";
+std::cout << "l6:" << f1.getLine(5) << " | w3:" << f1.getWord(2) << " | l3w2:" << f1.getWord(2, 1) << " | c1:" << f1.getChar(0)
+<< " | l2c3:" << f1.getChar(1, 2) << " | l4w3c5:" << f1.getChar(3, 2, 4) << "\n\n";
 
-std::cout << "c16-50:" << inFile.getChars(15, 49) << " | c50-16:" << inFile.getChars(49, 15) << " | l4c3-7:" << inFile.getChars(3, 2, 6)
-<< " | l4c7-3:" << inFile.getChars(3, 6, 2) << " | l4w2c2-5:" << inFile.getChars(3, 1, 1, 4) << " | l4w2c5-2:" << inFile.getChars(3, 1, 4, 1) << "\n\n";
-std::cout << "w2-4:" << inFile.getWords(1, 3) << " | w4-2:" << inFile.getWords(3, 1) << " | l2w2-4:" << inFile.getWords(1, 1, 3) << " | l2w4-2:" << inFile.getWords(1, 3, 1) << "\n\n";
+std::cout << "c16-50:" << f1.getChars(15, 49) << " | c50-16:" << f1.getChars(49, 15) << " | l4c3-7:" << f1.getChars(3, 2, 6)
+<< " | l4c7-3:" << f1.getChars(3, 6, 2) << " | l4w2c2-5:" << f1.getChars(3, 1, 1, 4) << " | l4w2c5-2:" << f1.getChars(3, 1, 4, 1) << "\n\n";
+std::cout << "w2-4:" << f1.getWords(1, 3) << " | w4-2:" << f1.getWords(3, 1) << " | l2w2-4:" << f1.getWords(1, 1, 3) << " | l2w4-2:" << f1.getWords(1, 3, 1) << "\n\n";
 */
 
 using Tstr = std::string;
@@ -80,7 +80,6 @@ void printSpaces(Tstr str) {
 		default:
 			std::cout << letter;
 		}
-		std::cout << ".";
 	}
 }
 
@@ -89,73 +88,81 @@ int main() {
 	char testChar;
 	Tstr test;
 
-	File inFile("input.txt");
-	File outFile("output.txt");
-	inFile.open();
-	outFile.open();
 
-	while (inFile.get(testChar)) {
-		std::cout << testChar;
-	}
+	File f1("f1.txt");
+	File f2("f2.txt");
+	File backup("backup.txt");
+	backup.move(f1);
+	backup.move(f2);
+	f1.close();
+	f1.open();
+	f2.open();
+	
+
+	f1.deleteLines(10, 2);
 
 
+	std::cout << "\n\n";
+	printSpaces(f1);
+	std::cout << "\n\n";
+	printSpaces(f2);
 
 	////FARE testare meglio
 	////add and delete
-	//inFile.addLine(0, "La-");				//l
-	//std::cout << inFile.getLine(0);
-	//inFile.deleteLine(0);
-	//inFile.addWord(1, "Wa1-");				//w
-	//std::cout << inFile.getWord(1);
-	//inFile.deleteWord(1);
-	//inFile.addWord(2, 2, "Wa2-");			//lw
-	//std::cout << inFile.getWord(2, 2);
-	//inFile.deleteWord(2, 2);
-	//inFile.addChar(0, 'A');					//c
-	//std::cout << inFile.getChar(0);
-	//inFile.deleteChar(0);
-	//inFile.addChar(1, 1, 'B');				//lc
-	//std::cout << inFile.getChar(1, 1);
-	//inFile.deleteChar(1, 1);
-	//inFile.addChar(2, 2, 0, 'C');			//lwc
-	//std::cout << inFile.getChar(2, 2, 0);
-	//inFile.deleteChar(2, 2, 0);
+	//f1.addLine(0, "La-");				//l
+	//std::cout << f1.getLine(0);
+	//f1.deleteLine(0);
+	//f1.addWord(1, "Wa1-");				//w
+	//std::cout << f1.getWord(1);
+	//f1.deleteWord(1);
+	//f1.addWord(2, 2, "Wa2-");			//lw
+	//std::cout << f1.getWord(2, 2);
+	//f1.deleteWord(2, 2);
+	//f1.addChar(0, 'A');					//c
+	//std::cout << f1.getChar(0);
+	//f1.deleteChar(0);
+	//f1.addChar(1, 1, 'B');				//lc
+	//std::cout << f1.getChar(1, 1);
+	//f1.deleteChar(1, 1);
+	//f1.addChar(2, 2, 0, 'C');			//lwc
+	//std::cout << f1.getChar(2, 2, 0);
+	//f1.deleteChar(2, 2, 0);
 	//
 	////replace
-	//test = inFile.getLine(1);				//l
-	//inFile.replaceLine(1, "Lr-");
-	//std::cout << inFile.getLine(1);
-	//inFile.replaceLine(1, test);
-	//test = inFile.getWord(1);				//w
-	//inFile.replaceWord(1, "Wr1-");
-	//std::cout << inFile.getWord(1);
-	//inFile.replaceWord(1, test);
-	//test = inFile.getWord(1, 0);			//lw
-	//inFile.replaceWord(1, 0, "Wr2-");
-	//std::cout << inFile.getWord(1, 0);
-	//inFile.replaceWord(1, 0, test);
-	//testChar = inFile.getChar(0);			//c
-	//inFile.replaceChar(0, 'D');
-	//std::cout << inFile.getChar(0);
-	//inFile.replaceChar(0, testChar);
-	//testChar = inFile.getChar(1, 1);		//lc
-	//inFile.replaceChar(1, 1, 'E');
-	//std::cout << inFile.getChar(1, 1);
-	//inFile.replaceChar(1, 1, testChar);
-	//testChar = inFile.getChar(2, 2, 0);		//lwc
-	//inFile.replaceChar(2, 2, 0, 'F');
-	//std::cout << inFile.getChar(2, 2, 0);
-	//inFile.replaceChar(2, 2, 0, testChar);
+	//test = f1.getLine(1);				//l
+	//f1.replaceLine(1, "Lr-");
+	//std::cout << f1.getLine(1);
+	//f1.replaceLine(1, test);
+	//test = f1.getWord(1);				//w
+	//f1.replaceWord(1, "Wr1-");
+	//std::cout << f1.getWord(1);
+	//f1.replaceWord(1, test);
+	//test = f1.getWord(1, 0);			//lw
+	//f1.replaceWord(1, 0, "Wr2-");
+	//std::cout << f1.getWord(1, 0);
+	//f1.replaceWord(1, 0, test);
+	//testChar = f1.getChar(0);			//c
+	//f1.replaceChar(0, 'D');
+	//std::cout << f1.getChar(0);
+	//f1.replaceChar(0, testChar);
+	//testChar = f1.getChar(1, 1);		//lc
+	//f1.replaceChar(1, 1, 'E');
+	//std::cout << f1.getChar(1, 1);
+	//f1.replaceChar(1, 1, testChar);
+	//testChar = f1.getChar(2, 2, 0);		//lwc
+	//f1.replaceChar(2, 2, 0, 'F');
+	//std::cout << f1.getChar(2, 2, 0);
+	//f1.replaceChar(2, 2, 0, testChar);
 
-	///*inFile.appendChar(' ');
-	//inFile.appendWord("");
-	//inFile.appendLine("\r\n");
-	//inFile.appendWord(0, "Ciao");*/
+	///*f1.appendChar(' ');
+	//f1.appendWord("");
+	//f1.appendLine("\r\n");
+	//f1.appendWord(0, "Ciao");*/
 
 	////FARE fa cose strane
-	//inFile.addLine(7, "Ciao");
-	//std::cout << "\n\n                         Intero file: " << inFile.getNrChars() << "byte\n";
-	//printSpaces(inFile);
+	//f1.addLine(7, "Ciao");
+	//std::cout << "\n\n                         Intero file: " << f1.getNrChars() << "byte\n";
+	//printSpaces(f1);
 
 
 	std::cout << "";
@@ -168,8 +175,8 @@ int main() {
 	std::cout << "";
 	std::cout << "";
 
-	inFile.close();
-	outFile.close();
+	f1.close();
+	f2.close();
 	askEnd();
 	return 0;
 }
